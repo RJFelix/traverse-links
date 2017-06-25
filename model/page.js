d361940be53dad87b3411096a269018462bda6bc
R F <a>
2017-06-24 21:35:58 -0700

    completed Page model

--- model/page.js
+++ model/page.js
@@ -1,9 +1,19 @@
 class Page {
   constructor({ title, content, url }) {
+    if(!(title && content && url)) {
+      throw new Error(`Page created without all parameters. Title: ${title}; Content: ${content}; url: ${url}`);
+    }
     this.title = title;
     this.content = content;
     this.url = url;
-    this.outgoingLinks = [];
+    this.links = [];
+  }
+
+  addLink(link) {
+    if(!(link instanceof Page)) {
+      throw new Error(`Link added to Page: ${title} was not a Page. Link: ${link}`);
+    }
+    this.links.push(link);
   }
 
 }
